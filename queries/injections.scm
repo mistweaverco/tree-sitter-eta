82ca1e692d1e0828187dc0dbd2f3ed6291294efb
Marco Kellershoff <marco@kellershoff.net>
2023-05-31 22:49:15 +0200

    Inherits in queries

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,15 +1,6 @@
+; inherits: html
+
 ((eta_script_element
   (raw_text) @injection.content)
  (#set! injection.language "javascript"))
 
-((script_element
-  (raw_text) @injection.content)
- (#set! injection.language "javascript"))
-
-((style_element
-  (raw_text) @injection.content)
- (#set! injection.language "css"))
-
-((text) @injection.content
- (#set! injection.language "html")
- (#set! injection.combined))
